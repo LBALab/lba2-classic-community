b009a5d419734ad196f3299891f161f1bd155dc8
Sergio Padrino <sergio.padrino@gmail.com>
2026-03-18 19:33:38 +0100

    test: validate rotmatu parity

--- tests/3D/test_rotmatu.cpp
+++ tests/3D/test_rotmatu.cpp
@@ -3,7 +3,6 @@
 #include <3D/ROTMAT.H>
 #include <3D/IMATSTD.H>
 #include <string.h>
-#include <stdlib.h>
 
 extern void RotateMatrixU(TYPE_MAT *d, TYPE_MAT *s, S32 x, S32 y, S32 z);
 
@@ -18,34 +17,119 @@ static void call_asm_RotateMatrixU(TYPE_MAT *d, TYPE_MAT *s, S32 x, S32 y, S32 z
         : "memory", "edx");
 }
 
+static U32 rng_state;
+
+static void rng_seed(U32 seed)
+{
+    rng_state = seed;
+}
+
+static U32 rng_next(void)
+{
+    rng_state = rng_state * 1103515245u + 12345u;
+    return (rng_state >> 16) & 0x7FFFu;
+}
+
+static TYPE_MAT make_identity_mat(void)
+{
+    TYPE_MAT m;
+    memset(&m, 0, sizeof(m));
+    m.F.M11 = 1.0f;
+    m.F.M22 = 1.0f;
+    m.F.M33 = 1.0f;
+    return m;
+}
+
+static TYPE_MAT make_test_mat(void)
+{
+    TYPE_MAT m;
+    memset(&m, 0, sizeof(m));
+    m.F.M11 = 0.5f;   m.F.M12 = 0.3f;   m.F.M13 = 0.1f;
+    m.F.M21 = 0.2f;   m.F.M22 = 0.8f;   m.F.M23 = 0.4f;
+    m.F.M31 = 0.7f;   m.F.M32 = 0.6f;   m.F.M33 = 0.9f;
+    return m;
+}
+
+static TYPE_MAT make_alt_test_mat(void)
+{
+    TYPE_MAT m;
+    memset(&m, 0, sizeof(m));
+    m.F.M11 = -0.75f; m.F.M12 = 0.25f;  m.F.M13 = 0.5f;
+    m.F.M21 = 0.125f; m.F.M22 = -0.5f;  m.F.M23 = 0.75f;
+    m.F.M31 = 0.625f; m.F.M32 = -0.25f; m.F.M33 = 0.375f;
+    return m;
+}
+
+static void fill_rand_mat(TYPE_MAT *m)
+{
+    float *values = &m->F.M11;
+    memset(m, 0, sizeof(*m));
+    for (int index = 0; index < 9; ++index) {
+        S32 sample = (S32)(rng_next() & 0x3FFu) - 512;
+        values[index] = (float)sample / 256.0f;
+    }
+}
+
+static void assert_rotmatu_case(const char *label, const TYPE_MAT *source_matrix, S32 x, S32 y, S32 z)
+{
+    TYPE_MAT cpp_src;
+    TYPE_MAT asm_src;
+    TYPE_MAT cpp_dst;
+    TYPE_MAT asm_dst;
+
+    memcpy(&cpp_src, source_matrix, sizeof(cpp_src));
+    memcpy(&asm_src, source_matrix, sizeof(asm_src));
+    memset(&cpp_dst, 0xCC, sizeof(cpp_dst));
+    memset(&asm_dst, 0xCC, sizeof(asm_dst));
+
+    RotateMatrixU(&cpp_dst, &cpp_src, x, y, z);
+    call_asm_RotateMatrixU(&asm_dst, &asm_src, x, y, z);
+
+    ASSERT_ASM_CPP_MEM_EQ(&asm_dst, &cpp_dst, sizeof(TYPE_MAT), label);
+    ASSERT_ASM_CPP_MEM_EQ(source_matrix, &cpp_src, sizeof(TYPE_MAT), label);
+    ASSERT_ASM_CPP_MEM_EQ(source_matrix, &asm_src, sizeof(TYPE_MAT), label);
+}
+
 static void test_equivalence(void)
 {
-    TYPE_MAT src; memset(&src,0,sizeof(src));
-    src.F.M11=1;src.F.M22=1;src.F.M33=1;
-    struct { S32 a,b,g; } cases[] = {
-        {0,0,0},{1024,0,0},{0,1024,0},{0,0,1024},{512,512,512},
+    TYPE_MAT matrices[] = {
+        make_identity_mat(),
+        make_test_mat(),
+        make_alt_test_mat(),
     };
-    for (int i = 0; i < (int)(sizeof(cases)/sizeof(cases[0])); i++) {
-        TYPE_MAT cm,am;
-        RotateMatrixU(&cm,&src,cases[i].a,cases[i].b,cases[i].g);
-        call_asm_RotateMatrixU(&am,&src,cases[i].a,cases[i].b,cases[i].g);
-        for (int j=0;j<9;j++)
-            ASSERT_ASM_CPP_NEAR_F((&am.F.M11)[j],(&cm.F.M11)[j],0.01f,"RotateMatrixU");
+    struct { S32 a,b,g; } cases[] = {
+        {0,0,0},
+        {1024,0,0},
+        {0,1024,0},
+        {0,0,1024},
+        {512,512,512},
+        {1023,1024,1025},
+        {-1,0,1},
+        {4096,-4096,8192},
+    };
+    for (int matrix_index = 0; matrix_index < (int)(sizeof(matrices)/sizeof(matrices[0])); ++matrix_index) {
+        for (int i = 0; i < (int)(sizeof(cases)/sizeof(cases[0])); i++) {
+            char lbl[128];
+            snprintf(lbl, sizeof(lbl), "RotateMatrixU fixed m=%d a=%d b=%d g=%d",
+                     matrix_index, cases[i].a, cases[i].b, cases[i].g);
+            assert_rotmatu_case(lbl, &matrices[matrix_index], cases[i].a, cases[i].b, cases[i].g);
+        }
     }
 }
 
 static void test_random_equivalence(void)
 {
-    srand(42);
-    TYPE_MAT src; memset(&src,0,sizeof(src));
-    src.F.M11=1;src.F.M22=1;src.F.M33=1;
-    for (int i = 0; i < 10000; i++) {
-        S32 a=rand()%4096, b=rand()%4096, g=rand()%4096;
-        TYPE_MAT cm,am;
-        RotateMatrixU(&cm,&src,a,b,g);
-        call_asm_RotateMatrixU(&am,&src,a,b,g);
-        for (int j=0;j<9;j++)
-            ASSERT_ASM_CPP_NEAR_F((&am.F.M11)[j],(&cm.F.M11)[j],0.01f,"RotateMatrixU rand");
+    rng_seed(0xDEADBEEFu);
+    for (int i = 0; i < 200; i++) {
+        TYPE_MAT src;
+        S32 a = (S32)(rng_next() % 16384u) - 8192;
+        S32 b = (S32)(rng_next() % 16384u) - 8192;
+        S32 g = (S32)(rng_next() % 16384u) - 8192;
+        char lbl[96];
+
+        fill_rand_mat(&src);
+        snprintf(lbl, sizeof(lbl), "RotateMatrixU rand a=%d b=%d g=%d", a, b, g);
+        assert_rotmatu_case(lbl, &src, a, b, g);
     }
 }
 
