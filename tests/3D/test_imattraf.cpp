55064b6303cb897f5a24834b44123b954644a920
Sergio Padrino <sergio.padrino@gmail.com>
2026-03-18 14:40:05 +0100

    Add test for InitMatrixTransF float translation slots
    Adds a unit test (tests/3D/test_imattraf.cpp) that verifies InitMatrixTransF stores translations in the matrix using the float bit representation. Introduces helper float_bits() to examine IEEE-754 bits and a buggy integer-overlay initializer to confirm the correct (float) behavior. The new test exercises several translation cases and asserts ASM/CPP memory equivalence and that integer-overlay values differ from the correct float representation.

--- tests/3D/test_imattraf.cpp
+++ tests/3D/test_imattraf.cpp
@@ -11,6 +11,21 @@ static void call_asm_InitMatrixTransF(TYPE_MAT *m, S32 tx, S32 ty, S32 tz) {
     __asm__ __volatile__("call asm_InitMatrixTransF" : : "D"(m), "a"(tx), "b"(ty), "c"(tz) : "memory");
 }
 
+static U32 float_bits(float value)
+{
+    U32 bits;
+    memcpy(&bits, &value, sizeof(bits));
+    return bits;
+}
+
+static void initmatrixtrans_buggy_integer_overlay(TYPE_MAT *m, S32 tx, S32 ty, S32 tz)
+{
+    memset(m, 0, sizeof(*m));
+    m->I.TX = tx;
+    m->I.TY = ty;
+    m->I.TZ = tz;
+}
+
 static void test_equivalence(void)
 {
     struct { S32 tx,ty,tz; } cases[] = {
@@ -38,10 +53,44 @@ static void test_random_equivalence(void)
     }
 }
 
+static void test_translation_slots_use_float_representation(void)
+{
+    struct { S32 tx, ty, tz; } cases[] = {
+        {1, -2, 1024},
+        {7, 255, -4096},
+        {-1234567, 7654321, 32768},
+    };
+
+    for (int i = 0; i < (int)(sizeof(cases) / sizeof(cases[0])); ++i) {
+        TYPE_MAT cm;
+        TYPE_MAT am;
+        TYPE_MAT buggy;
+
+        memset(&cm, 0xCC, sizeof(cm));
+        memset(&am, 0xCC, sizeof(am));
+
+        InitMatrixTransF(&cm, cases[i].tx, cases[i].ty, cases[i].tz);
+        call_asm_InitMatrixTransF(&am, cases[i].tx, cases[i].ty, cases[i].tz);
+        initmatrixtrans_buggy_integer_overlay(&buggy, cases[i].tx, cases[i].ty, cases[i].tz);
+
+        ASSERT_ASM_CPP_MEM_EQ(&am, &cm, sizeof(TYPE_MAT), "InitMatrixTransF float translation slots");
+        ASSERT_EQ_UINT(float_bits((float)cases[i].tx), (U32)cm.I.TX);
+        ASSERT_EQ_UINT(float_bits((float)cases[i].ty), (U32)cm.I.TY);
+        ASSERT_EQ_UINT(float_bits((float)cases[i].tz), (U32)cm.I.TZ);
+        ASSERT_EQ_UINT((U32)cm.I.TX, (U32)am.I.TX);
+        ASSERT_EQ_UINT((U32)cm.I.TY, (U32)am.I.TY);
+        ASSERT_EQ_UINT((U32)cm.I.TZ, (U32)am.I.TZ);
+        ASSERT_TRUE((U32)buggy.I.TX != (U32)cm.I.TX);
+        ASSERT_TRUE((U32)buggy.I.TY != (U32)cm.I.TY);
+        ASSERT_TRUE((U32)buggy.I.TZ != (U32)cm.I.TZ);
+    }
+}
+
 int main(void)
 {
     RUN_TEST(test_equivalence);
     RUN_TEST(test_random_equivalence);
+    RUN_TEST(test_translation_slots_use_float_representation);
     TEST_SUMMARY();
     return test_failures != 0;
 }
